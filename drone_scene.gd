9dd25f776c8658746a32b72e26d1a351669392b4
JoeM <C21404162@mytudublin.ie>
2025-04-29 09:51:16 +0100

    Camera following

--- drone_scene.gd
+++ drone_scene.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
 @export var drone_scene: PackedScene
+@onready var camera = $Camera3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,6 +9,7 @@ func _ready() -> void:
 	var drone = drone_scene.instantiate()
 	add_child(drone)
 	drone.position = Vector3(0, 1, 0)
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
