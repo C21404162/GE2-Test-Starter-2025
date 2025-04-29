631d54daa727d7eb311c5b61a6269223b0a6a43f
JoeM <C21404162@mytudublin.ie>
2025-04-29 10:34:28 +0100

    drone camera kinda working

--- camera_3d.gd
+++ camera_3d.gd
@@ -1,5 +1,7 @@
 extends Camera3D
 
+@onready var drone = $".."
+
 #ADD LERP HERE
 
 # Called when the node enters the scene tree for the first time.
@@ -9,4 +11,9 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	var drone_forward = -drone.global_transform.basis.z
+	var target_pos = drone.global_position + (drone_forward * 4.0) + Vector3(0, 3, 0)    
+	global_position = global_position.lerp(target_pos, 5.0 * delta)
+		
+	look_at(drone.global_position)
+	#pass
--- drone.gd
+++ drone.gd
@@ -1,23 +1,23 @@
 extends RigidBody3D
 
-@export var lift_force: float = 20
-@export var move_force: float = 5
-@export var rotation_force: float = .01
+@export var lift_power: float = 20
+@export var move: float = 5
+@export var rot: float = .01
 
 func _physics_process(delta: float) -> void:
 	var lift = Input.is_action_pressed("up") 
-	var forward = Input.get_axis("backward", "forward") 
-	var turn = Input.get_axis("left", "right")  
+	var forward = Input.get_axis("forward", "reverse") 
+	var turn = Input.get_axis("right", "left")  
 	if lift:
 		print("LIFTING")
-		apply_central_force(Vector3.UP * lift_force)
+		apply_central_force(Vector3.UP * lift_power)
 	
 	if forward != 0:
 		print("MOVING FORWARD")
-		apply_central_force(-global_transform.basis.z * forward * move_force)
+		apply_central_force(-global_transform.basis.z * forward * move)
 	
 	#too fast
-	
 	if turn != 0:
+		#spin props
 		print("TURNING")
-		apply_torque_impulse(Vector3.UP * turn * rotation_force)
+		apply_torque_impulse(Vector3.UP * turn * rot)
