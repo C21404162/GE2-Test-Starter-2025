c7088b1c3691f867dd4ebdb7d17d8c44e13c9670
JoeM <C21404162@mytudublin.ie>
2025-04-29 10:00:18 +0100

    Cam+Drone changes

--- drone.gd
+++ drone.gd
@@ -1,11 +1,18 @@
 extends RigidBody3D
 
+@export var lift_power: float = 1.0
+@export var move: float = 1.0
+@export var rot: float = 1.0
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _physics_process(delta: float) -> void:
+	var lift = Input.get_action_strength("up")
+	var forward = Input.get_action_strength("forward") - Input.get_action_strength("reverse")
+	var turn = Input.get_action_strength("right") - Input.get_action_strength("left")
+	if lift > 0:
+		apply_central_force(Vector3.UP * lift * lift_power)
+	
+	if forward != 0:
+		apply_central_force(-global_transform.basis.z * forward * move)
+	
+	if turn != 0:
+		apply_torque_impulse(Vector3.UP * turn * rot)
