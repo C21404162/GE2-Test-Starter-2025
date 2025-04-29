b669ebb7815a02a691c353a1b97bc4643f8acdaf
JoeM <C21404162@mytudublin.ie>
2025-04-29 10:07:42 +0100

    Drone kinda controls+big boxes for ref

--- drone.gd
+++ drone.gd
@@ -1,18 +1,21 @@
 extends RigidBody3D
 
-@export var lift_power: float = 1.0
-@export var move: float = 1.0
-@export var rot: float = 1.0
+@export var lift_force: float = 20
+@export var move_force: float = 5
+#@export var rotation_force: float = 1
 
 func _physics_process(delta: float) -> void:
-	var lift = Input.get_action_strength("up")
-	var forward = Input.get_action_strength("forward") - Input.get_action_strength("reverse")
-	var turn = Input.get_action_strength("right") - Input.get_action_strength("left")
-	if lift > 0:
-		apply_central_force(Vector3.UP * lift * lift_power)
+	var lift = Input.is_action_pressed("up") 
+	var forward = Input.get_axis("backward", "forward") 
+	var turn = Input.get_axis("left", "right")  
+	if lift:
+		print("LIFTING")
+		apply_central_force(Vector3.UP * lift_force)
 	
 	if forward != 0:
-		apply_central_force(-global_transform.basis.z * forward * move)
+		print("MOVING FORWARD")
+		apply_central_force(-global_transform.basis.z * forward * move_force)
 	
-	if turn != 0:
-		apply_torque_impulse(Vector3.UP * turn * rot)
+	#too fast
+	#if turn != 0:
+		#apply_torque_impulse(Vector3.UP * turn * rotation_force)
